304336e8e4df92708dffa1da1cdfdf4254728647
marlonchiu <Knight1898@outlook.com>
2020-12-11 00:51:47 +0800

    6.4 ~ 6.6 销售趋势组件开发

--- imooc-datav-report-dev/src/components/SalesView/index.vue
+++ imooc-datav-report-dev/src/components/SalesView/index.vue
@@ -1,14 +1,271 @@
 <template>
-  <div>
-    SalesView
+  <div class="sales-view">
+    <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
+      <template slot="header">
+        <div class="menu-wrapper">
+          <el-menu mode="horizontal" :default-active="activeIndex" @select="onMenuSelect" class="sales-view-menu">
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="今年"></el-radio-button>
+            </el-radio-group>
+            <el-date-picker
+              v-model="date"
+              size="small"
+              type="daterange"
+              unlink-panels
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              :picker-options="pickerOptions"
+              class="sales-view-date-picker"
+            ></el-date-picker>
+          </div>
+        </div>
+      </template>
+      <template>
+        <div class="sales-view-chart-wrapper">
+          <v-chart :options="chartOption" />
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div class="list-item" v-for="item in rankData" :key="item.no">
+                <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{ item.no }}</div>
+                <div class="list-item-name">{{ item.name }}</div>
+                <div class="list-item-money">{{ item.money }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SalesView'
+  name: 'SalesView',
+  data () {
+    return {
+      activeIndex: '1',
+      radioSelect: '今日',
+      date: null,
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近三个月',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      },
+      chartOption: {
+        title: {
+          text: '年度销售业绩数据',
+          textStyle: {
+            fontSize: 12,
+            color: '#666'
+          },
+          left: 25,
+          top: 20
+        },
+        xAxis: {
+          type: 'category',
+          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+          axisTick: {
+            alignWithLabel: true,
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          // 短线
+          axisLine: {
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          axisLabel: {
+            color: '#333'
+          }
+        },
+        yAxis: {
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          splitLine: {
+            lineStyle: {
+              type: 'dotted',
+              color: '#eee'
+            }
+          }
+        },
+        series: [{
+          type: 'bar',
+          barWidth: '35%',
+          data: ['330', '420', '560', '450', '610', '890', '720', '610', '580', '750', '770', '600']
+        }],
+        color: ['#3398DB'],
+        grid: {
+          top: 70,
+          left: 60,
+          right: 60,
+          bottom: 50
+        }
+      },
+      orderRank: [
+        { no: 1, name: '肯德基', money: '323,234' },
+        { no: 2, name: '麦当劳', money: '299,132' },
+        { no: 3, name: '肯德基', money: '283,998' },
+        { no: 4, name: '海底捞', money: '266,223' },
+        { no: 5, name: '西贝筱面村', money: '223,445' },
+        { no: 6, name: '汉堡王', money: '219,663' },
+        { no: 7, name: '真功夫', money: '200,997' }
+      ],
+      userRank: [
+        { no: 1, name: '麦当劳', money: '211,335' },
+        { no: 2, name: '肯德基', money: '210,597' },
+        { no: 3, name: '必胜客', money: '200,998' },
+        { no: 4, name: '海底捞', money: '199,220' },
+        { no: 5, name: '西贝筱面村', money: '195,444' },
+        { no: 6, name: '汉堡王', money: '180,161' },
+        { no: 7, name: '真功夫', money: '172,995' }
+      ]
+    }
+  },
+  computed: {
+    rankData () {
+      return this.activeIndex === '1' ? this.orderRank : this.userRank
+    }
+  },
+  methods: {
+    onMenuSelect (index) {
+      this.activeIndex = index
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.sales-view {
+  margin-top: 20px;
+
+  .menu-wrapper {
+    position: relative;
+    display: flex;
+
+    .sales-view-menu {
+      width: 100%;
+      padding-left: 20px;
+
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+
+    .menu-right {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      .sales-view-date-picker {
+        margin-left: 20px;
+      }
+    }
+  }
+
+  .sales-view-chart-wrapper {
+    display: flex;
+    height: 270px;
+
+    .echarts {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+
+    .sales-view-list {
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+
+      .sales-view-title {
+        margin-top: 20px;
+        font-size: 12px;
+        color: #666;
+        font-weight: 500;
+      }
+
+      .list-item-wrapper {
+        margin-top: 15px;
+
+        .list-item {
+          display: flex;
+          align-items: center;
+          font-size: 12px;
+          height: 20px;
+          padding: 6px 20px 6px 0;
+
+          .list-item-no {
+            width: 20px;
+            height: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #333;
+
+            &.top-no {
+              background: #000;
+              border-radius: 50%;
+              color: #fff;
+              font-weight: 500;
+            }
+          }
+
+          .list-item-name {
+            margin-left: 10px;
+            color: #333;
+          }
+
+          .list-item-money {
+            flex: 1;
+            text-align: right;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
--- imooc-datav-report-dev/src/plugins/element.js
+++ imooc-datav-report-dev/src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button, Card, Row, Col } from 'element-ui'
+import {
+  Button, Card, Row, Col, Menu, MenuItem,
+  RadioGroup, RadioButton, DatePicker, Table, TableColumn, Pagination
+} from 'element-ui'
 
-Vue.use(Button).use(Card).use(Row).use(Col)
+Vue.use(Button).use(Card).use(Row).use(Col).use(Menu).use(MenuItem)
+  .use(RadioGroup).use(RadioButton).use(DatePicker).use(Table).use(TableColumn).use(Pagination)
