59dffe9555639f5516821f857af83a166deb5399
marlonchiu <Knight1898@outlook.com>
2020-12-10 22:02:56 +0800

    vue-echarts v-charts组件库入门

--- imooc-datav-report-dev/src/main.js
+++ imooc-datav-report-dev/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './plugins/vcharts.js'
 import ECharts from 'echarts'
+import VueCharts from 'vue-echarts'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts
+Vue.component('v-chart', VueCharts)
 
 new Vue({
   router,
