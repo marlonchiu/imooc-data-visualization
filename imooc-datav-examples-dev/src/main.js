0ed93001f35889f86cdeb1f89704affe6c4bf639
marlonchiu <Knight1898@outlook.com>
2020-12-13 17:00:26 +0800

    8-5 水球图的基本用法

---
+++ imooc-datav-examples-dev/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ECharts from 'echarts'
+
+Vue.config.productionTip = false
+Vue.prototype.$echarts = ECharts
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ imooc-datav-examples-dev/src/views/Liquidfill.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script>
+import 'echarts-liquidfill'
+export default {
+  name: 'Liquidfill',
+  mounted () {
+    const data = [0.6, {
+      value: 0.4,
+      direction: 'left'
+    }, {
+        value: 0.2,
+        itemStyle: {
+          opacity: 0.6
+        },
+        emphasis: {
+          itemStyle: {
+            opacity: 0.9
+          }
+        }
+      }]
+    const chart = this.$echarts.init(document.getElementById('container'))
+    chart.setOption({
+      series: [{
+        type: 'liquidFill',
+        data: data,
+        color: ['red', 'blue', 'yellow'],
+        // waveAnimation: false,
+        // animationDuration: 0,
+        // animationDurationUpdate: 0,
+        amplitude: 10,
+        shape: 'ract',
+        // backgroundStyle: {
+        //   color: 'purple',
+        //   borderWidth: 4,
+        //   borderColor: '#333'
+        // },
+        outline: {
+          show: true,
+          borderDistance: 2,
+          itemStyle: {
+            borderWidth: 2,
+            borderColor: 'red',
+            shadowBlur: 'none'
+          }
+        }
+      }]
+
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#container {
+  width: 100%;
+  height: 100%;
+}
+</style>
