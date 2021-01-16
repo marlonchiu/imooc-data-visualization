0bf5d61583ec8300f47fa2c338a5b4fb6ba3619b
marlonchiu <Knight1898@outlook.com>
2021-01-16 11:30:21 +0800

    第23章 echarts 按需引入

--- imooc-datav-mobile-dev/src/libs/echarts.js
+++ imooc-datav-mobile-dev/src/libs/echarts.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import * as ECharts from 'echarts'
+// import * as echarts from 'echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/pie'
+import 'echarts/lib/chart/sunburst'
+import 'echarts/lib/chart/radar'
+
 import VueECharts from 'vue-echarts'
 
-Vue.prototype.$echarts = ECharts
+import 'echarts/lib/component/grid'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/tooltip'
+
+// Vue.prototype.$echarts = echarts
+
 Vue.component('vue-echarts', VueECharts)
 Vue.prototype.$bmap = window.BMapGL
 Vue.prototype.$initMap = window.initMap
